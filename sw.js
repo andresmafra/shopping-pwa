497d92c30714e3832671db83ae8e241537fb1439
André Mafra <andresmafra@gmail.com>
2017-05-25 18:38:38 -0300

    cache

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@ this.addEventListener('fetch', function(event) {
 
 this.addEventListener('install', function(event) {
   event.waitUntil(
-    caches.open('v1').then(function(cache) {
+    caches.open('v2').then(function(cache) {
       return cache.addAll([
         'index.html',
         'loja-adidas.html',
