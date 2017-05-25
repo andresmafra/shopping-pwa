583738b032346c3d79f9fef797d31a0cba2a0f3d
André Mafra <andresmafra@gmail.com>
2017-05-25 18:25:58 -0300

    spa

--- js/main.js
+++ js/main.js
@@ -14,3 +14,13 @@ $(document).on('pageload', function(){
     $('.code-scan').hide();
   }
 });
+
+
+// var connection = navigator.connection || navigator.mozConnection || navigator.webkitConnection;
+// var type = connection.type;
+
+// function updateConnectionStatus() {
+//   console.log("Connection type is change from " + type + " to " + connection.type);
+// }
+
+// connection.addEventListener('typechange', updateConnectionStatus);
--- js/spa.js
+++ js/spa.js
@@ -6,3 +6,18 @@
 	Bonus points: faça uma transição de tela com CSS entre um estado e outro!
 
  */
+
+ $('a').on('click', function(e) {
+     e.preventDefault();
+     var link = $(this).attr('href')
+
+     $('.current-page').load(link + ' .current-page' , function() {
+          history.pushState({}, {}, e.target.href);
+    });
+ });
+
+window.addEventListener("popstate", function(e) {
+    $(".current-page").load(location.pathname + ' .current-page' , function() {
+          history.pushState({}, {}, e.target.href);
+    });
+});
