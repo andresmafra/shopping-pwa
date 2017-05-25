61d68d991cb3e6d677265d4fadf804813315da73
André Mafra <andresmafra@gmail.com>
2017-05-25 18:41:43 -0300

    off

--- js/pagamento.js
+++ js/pagamento.js
@@ -6,29 +6,38 @@
  */
 
 
-if(!!window.location.href.match('pagar-estacionamento')) {
-   if(!navigator.onLine) {
-  alert('Fique online para realizar o pagamento!');
-  $('form button').attr('disabled', 'disabled')
-}
-}
+// if(!!window.location.href.match('pagar-estacionamento')) {
+//    if(!navigator.onLine) {
+//   alert('Fique online para realizar o pagamento!');
+//   $('form button').attr('disabled', 'disabled')
+// }
+// }
 
 
 $('body').on('submit', 'form', finalizaCompra);
 
 function finalizaCompra(){
-  Materialize.toast('Só testes, não enviei o cartão, claro', 4000);
-  $('form')[0].reset();
 
-  var now = new Date();
-  var saida = new Date(now.getTime() + 1000 * 60 * 15);
+   if(!navigator.onLine) {
+    alert('Fique online para realizar o pagamento!');
+    $('form button').attr('disabled', 'disabled')
+  } else {
 
-  var options = {
-      icon: 'img/icon.png',
+    Materialize.toast('Só testes, não enviei o cartão, claro', 4000);
+    $('form')[0].reset();
+
+    var now = new Date();
+    var saida = new Date(now.getTime() + 1000 * 60 * 15);
+
+    var options = {
+        icon: 'img/icon.png',
+    }
+
+    var notification = new Notification('Pagamento confirmado! Saída liberada até ' + saida.getHours() + 'h' + ("0" + (saida.getMinutes() + 1)).slice(-2), options);
+    setTimeout(n.close.bind(notification), 2500); 
   }
 
-  var notification = new Notification('Pagamento confirmado! Saída liberada até ' + saida.getHours() + 'h' + ("0" + (saida.getMinutes() + 1)).slice(-2), options);
-  setTimeout(n.close.bind(notification), 2500); 
+  
 
 	return false;
 }
