9b3796d9a3cdc8d4d3a25569b337bc759a5c55f8
André Mafra <andresmafra@gmail.com>
2017-05-25 18:09:27 -0300

    updates

--- js/install.js
+++ js/install.js
@@ -3,3 +3,12 @@
   Desafio: use esse arquivo pra disparar a instalação do Service Worker.
   
  */
+
+ if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/sw.js', {scope: '/'})
+  .then(function(reg) {
+    console.log('Success on SW! ' + reg.scope);
+  }).catch(function(error) {
+    console.log('Error on SW ' + error);
+  });
+}
