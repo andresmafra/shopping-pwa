2d80a3ac05e8a12edb6a81d5f1050acfada94d4b
André Mafra <andresmafra@gmail.com>
2017-05-26 09:14:48 -0300

    update on payment

--- js/pagamento.js
+++ js/pagamento.js
@@ -3,44 +3,68 @@
   Desafio: implemente a PaymentRequest API se estiver disponível, ao 
   invés de usar o formulário padrão da página.
 
- */
+  */
 
 
-// if(!!window.location.href.match('pagar-estacionamento')) {
-//    if(!navigator.onLine) {
-//   alert('Fique online para realizar o pagamento!');
-//   $('form button').attr('disabled', 'disabled')
-// }
-// }
+  if(window.PaymentRequest) {  
 
+    const supportedPaymentMethods = [  
+    {  
+      supportedMethods: ['basic-card'],  
+    }  
+    ];  
+    const paymentDetails = {  
+      total: {
+        label: 'Total',  
+        amount:{  
+          currency: 'BRL',  
+          value: 10.00  
+        }  
+      }
+    };
 
-$('body').on('submit', 'form', finalizaCompra);
+    new PaymentRequest(  
+      supportedPaymentMethods,  
+      paymentDetails,  
+      {}  
+      ).show().then(function(data) {
+        console.log(data)
+      }).catch(function(err) {
+        console.log(err)
+      });
 
-function finalizaCompra(){
+    } else {  
 
-   if(!navigator.onLine) {
-    alert('Fique online para realizar o pagamento!');
-    $('form button').attr('disabled', 'disabled');
-    return false;
-  } else {
+      $('body').on('submit', 'form', finalizaCompra);
 
-    Materialize.toast('Só testes, não enviei o cartão, claro', 4000);
-    $('form')[0].reset();
+      function finalizaCompra(){
 
-    var now = new Date();
-    var saida = new Date(now.getTime() + 1000 * 60 * 15);
+       if(!navigator.onLine) {
+        alert('Fique online para realizar o pagamento!');
+        $('form button').attr('disabled', 'disabled');
+        return false;
+      } else {
 
-    var options = {
-        icon: 'img/icon.png',
+        Materialize.toast('Só testes, não enviei o cartão, claro', 4000);
+        $('form')[0].reset();
+
+        var now = new Date();
+        var saida = new Date(now.getTime() + 1000 * 60 * 15);
+
+        var options = {
+          icon: 'img/icon.png',
+        }
+
+        var notification = new Notification('Pagamento confirmado! Saída liberada até ' + saida.getHours() + 'h' + ("0" + (saida.getMinutes() + 1)).slice(-2), options);
+        setTimeout(notification.close() , 2500); 
+        console.log('returnnnn')
+        return false;
+      }
+
     }
 
-    var notification = new Notification('Pagamento confirmado! Saída liberada até ' + saida.getHours() + 'h' + ("0" + (saida.getMinutes() + 1)).slice(-2), options);
-    setTimeout(n.close.bind(notification), 2500); 
-
-    return false;
   }
 
-  
 
-	
-}
+
+
